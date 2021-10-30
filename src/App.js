a730147fce4c6e1a57cf4603e05bac2702d33c03
Faiz Taj Moahmmed <90762914+faiztaj0502@users.noreply.github.com>
2021-10-30 19:30:55 +0530

    Initial code check-in

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { RegistrationForm } from "./components/RegistrationForm";
+
+function App() {
+  return (
+    <div className="app">
+      <center>
+        <h1>Registration Form</h1>
+      </center>
+      <RegistrationForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { Formik, Form } from "formik";
+import { InputFields } from "./InputFields";
+import * as Yup from "yup";
+
+export const RegistrationForm = () => {
+  const phonenumber =
+    /^[\+]?[(]?[0-9]{3}[)]?[-\s\]?[0-9]{3}[-\s\]?[0-9]{4,6}$/im;
+  const validate = Yup.object({
+    firstName: Yup.string()
+      .min(3, "Must be 3 characters or less")
+      .max(16, "Must be 16 characters or less")
+      .required("Required *"),
+    lastName: Yup.string()
+      .min(3, "Must be 3 characters or less")
+      .max(16, "Must be 16 characters or less")
+      .required("Required *"),
+    email: Yup.string().email("Email is invalid").required("Email is required"),
+    contact: Yup.string()
+      .min(10, "Enter valid mobile number")
+      .required("Phone number is required *")
+      .matches(phonenumber, "Inter valid mobile number"),
+    password: Yup.string()
+      .min(6, "Password must be at least 6 charaters")
+      .required("Password is required *"),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref("password"), null], "Password must match")
+      .required("Confirm password is required *"),
+  });
+  return (
+    <Formik
+      initialValues={{
+        firstName: "",
+        lastName: "",
+        email: "",
+        contact: "",
+        password: "",
+        confirmPassword: "",
+      }}
+      validationSchema={validate}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      {(formik) => (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Form style={{ width: "20rem" }}>
+            <InputFields label="Name" name="name" type="text" />
+            <InputFields label="Username" name="username" type="text" />
+            <InputFields label="Password" name="password" type="password" />
+            <InputFields label="Contact" name="contact" type="" />
+            <InputFields label="Employee ID" name="id" type="text" />
+            <InputFields
+              label="Role"
+              name="role"
+              type="text"
+            />
+            <button
+              className="btn btn-dark mt-3"
+              type="submit"
+              disabled={!(formik.isValid && formik.dirty)}
+            >
+              Submit
+            </button>
+            {/* <button className="btn btn-danger mt-3 ml-3" type="reset">Reset</button> */}
+          </Form>
+        </div>
+      )}
+    </Formik>
+  );
+};
